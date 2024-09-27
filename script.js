30430b5d74b9f4feefa25e73478600787c123aeb
exesinan <sinankaraboga42@gmail.com>
2024-09-27 11:35:05 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll effect for navbar
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
